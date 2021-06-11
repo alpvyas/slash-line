7c8d0dddda93e9b359f36f96f93f16f8ad0649c1
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-11 16:59:40 -0700

    child components set up

---
+++ react-app/src/components/GettingStarted/Success/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
+import './Success.css';
+
+const Success = () => {
+
+  const [step, setStep] = useState("first");
+
+  return (
+    <>
+      <div className="card">
+        <h2>You're all set!</h2>
+        <h2>Make sure you sign players to your team.</h2>
+        <div className="buttons-container">
+          <Button className="new-league-button" variant="outlined" color="primary" onClick={() => setStep("joinRandomLeague")}>
+            Complete Setup
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Success;
--- react-app/src/components/GettingStarted/index.js
+++ react-app/src/components/GettingStarted/index.js
@@ -6,6 +6,8 @@ import Step1 from './Step1';
 import JoinLeague from './JoinLeague';
 import JoinUserLeague from './JoinUserLeague';
 import CreateTeam from './CreateTeam';
+import Success from './Success';
+import OpenLeagues from './OpenLeagues';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import ball_in_glove from '../../images/baseball-glove-dirt.png';
@@ -23,10 +25,14 @@ const GettingStarted = () => {
           <NavBar />
         </div>
         <div className="middle-container">
-          {/* <Step1 /> */}
-          {/* <JoinLeague /> */}
-          {/* <JoinUserLeague /> */}
-          <CreateTeam />
+          <form>
+            <Step1 currentStep={step} setStep={setStep}/>
+          </form>
+          {/* <JoinLeague currentStep={step} setStep={setStep}/> */}
+          {/* <JoinUserLeague currentStep={step} setStep={setStep}/> */}
+          {/* <CreateTeam currentStep={step} setStep={setStep}/> */}
+          {/* <Success currentStep={step} setStep={setStep}/> */}
+          {/* <OpenLeagues currentStep={step} setStep={setStep}/> */}
         </div>
         <div className="footer-container">
           <Footer />
