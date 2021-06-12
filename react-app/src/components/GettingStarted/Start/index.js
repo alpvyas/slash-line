204e8b610b20fdae92ac99ded1b392ef813cddb3
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-11 19:44:35 -0700

    getting started is mostly set, need to fix button onClicks and form submission...and styling

--- react-app/src/components/GettingStarted/Start/index.js
+++ react-app/src/components/GettingStarted/Start/index.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import './Step1.css';
+import './Start.css';
 
 const Step1 = (step, setStep, join, setJoin) => {
 
   return (
     <>
       <div className="card">
-        <h2>{`Let's Get Started! This is the step ${step}`}</h2>
+        <h2>Let's Get Started!</h2>
         <div className="buttons-container">
           <Button className="join-button" variant="outlined" color="primary" onClick={() => setStep("joinLeague")}>
             Join A League
