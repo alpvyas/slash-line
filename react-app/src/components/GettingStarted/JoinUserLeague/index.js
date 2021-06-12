34223b3c3f4dd05fb266d716f9ab7e6c40457bc9
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-11 22:56:47 -0700

    button functionality coming together

--- react-app/src/components/GettingStarted/JoinUserLeague/index.js
+++ react-app/src/components/GettingStarted/JoinUserLeague/index.js
@@ -6,23 +6,33 @@ import './JoinUserLeague.css';
 
 const JoinUserLeague = () => {
 
-  const [step, setStep] = useState("first");
+  const [credentials, setCredentials] = useState(undefined);
 
   return (
-    <>
+   
       <div className="card">
-        <h2>Great! Enter the League ID and passcode below</h2>
-        <form className="input-container">
-          <TextField variant="outlined" label="League ID"></TextField>
-          <TextField variant="outlined" label="Passcode"></TextField>
-        </form>
-        <div className="buttons-container">
-          <Button className="new-league-button" variant="outlined" color="primary" onClick={() => setStep("joinRandomLeague")}>
-            Next
-          </Button>
-        </div>
+
+        {credentials===undefined && (
+          <>
+            <h2>Great! Enter the League ID and passcode below</h2>
+
+            <form className="input-container">
+              <TextField variant="outlined" label="League ID"></TextField>
+              <TextField variant="outlined" label="Passcode"></TextField>
+            </form>
+
+            <div className="buttons-container">
+
+              <Button className="new-league-button" variant="outlined" color="primary" onClick={() => setCredentials(true)}>
+                Join this league
+              </Button>
+
+            </div>
+          </>
+        )}
+
       </div>
-    </>
+   
   );
 };
 
--- react-app/src/components/GettingStarted/Start/index.js
+++ react-app/src/components/GettingStarted/Start/index.js
@@ -2,26 +2,40 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import JoinLeague from '../JoinLeague';
 import './Start.css';
 
-const Step1 = (step, setStep, join, setJoin) => {
+const Start = () => {
 
+  const [action, setAction] = useState("none");
+  
   return (
-    <>
-      <div className="card">
-        <h2>Let's Get Started!</h2>
-        <div className="buttons-container">
-          <Button className="join-button" variant="outlined" color="primary" onClick={() => setStep("joinLeague")}>
-            Join A League
-          </Button>
+    
+      <div className="card"> 
 
-          <Button className="new-league-button" variant="outlined" color="primary" onClick={() => setStep("newLeague")}>
-            Start a New League
-          </Button>
-        </div>
+
+        {action === "none" && (
+          <>
+          <h2>Let's Get Started!</h2>
+          <div className="buttons-container">
+            <Button className="join-button" variant="outlined" color="primary" onClick={() => setAction("join") }>
+              Join A League
+            </Button>
+
+            <Button className="new-league-button" variant="outlined" color="primary" >
+              Start a New League
+            </Button>
+          </div> 
+        </>
+        )}
+
+
+        {action==="join" && <JoinLeague />}
+        {/* {action==="create" && <CreateLeague />} */}
+
       </div> 
-    </>
+   
   );
 };
 
-export default Step1;
+export default Start;
