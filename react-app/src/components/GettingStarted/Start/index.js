ebd439a2f205677d4572937a38683db849b0d6aa
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-11 18:17:05 -0700

    redoing gettingstarted

--- react-app/src/components/GettingStarted/Start/index.js
+++ react-app/src/components/GettingStarted/Start/index.js
@@ -4,14 +4,12 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import './Step1.css';
 
-const Step1 = () => {
-
-  const [step, setStep] = useState("first");
+const Step1 = (step, setStep, join, setJoin) => {
 
   return (
     <>
       <div className="card">
-        <h2>Let's Get Started!</h2>
+        <h2>{`Let's Get Started! This is the step ${step}`}</h2>
         <div className="buttons-container">
           <Button className="join-button" variant="outlined" color="primary" onClick={() => setStep("joinLeague")}>
             Join A League
--- react-app/src/components/GettingStarted/index.js
+++ react-app/src/components/GettingStarted/index.js
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import Step1 from './Step1';
+import Start from './Start';
 import JoinLeague from './JoinLeague';
 import JoinUserLeague from './JoinUserLeague';
+import OpenLeagues from './OpenLeagues';
 import CreateTeam from './CreateTeam';
 import Success from './Success';
-import OpenLeagues from './OpenLeagues';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import ball_in_glove from '../../images/baseball-glove-dirt.png';
 import './GettingStarted.css';
 
+const setPages = [Start, JoinLeague, JoinUserLeague, OpenLeagues, CreateTeam, Success, ]
+
 
 const GettingStarted = () => {
 
-  const [step, setStep] = useState("first");
+  const [step, setStep] = useState(0);
+  const [formState, setFormState] = useState({});
+  const [steps, setSteps] = useState([
+    { label: "Start", isValid: undefined},
+    { label: "Join a League", isValid: undefined},
+    { label: "Create Your Team", isValid: undefined},
+    { label: "Confirm", isValid: undefined},
+  ]);
+
+  const lastStepIndex = steps.length - 1;
+  const isLastStep = lastStepIndex === step;
+  const isPreviousStepValid = steps.slice(0, step)
+                                   .findIndex(
+
+    currentStep => currentStep.isValid === false
+    
+    ) === -1;
+
+    const onStepSubmit = useCallback(
+      event => {
+        const { isValid, values } = event;
+
+        const currentSteps = steps.map((currentStep, index) => ({
+          ...currentStep,
+          isValid: index === step ? isValid : currentStep.isValid,
+        }));
+      });
+
 
   return (
     <>
@@ -26,7 +55,7 @@ const GettingStarted = () => {
         </div>
         <div className="middle-container">
           <form>
-            <Step1 currentStep={step} setStep={setStep}/>
+            <Step1 step={step} setStep={setStep} join={join} setJoin={setJoin}/>
           </form>
           {/* <JoinLeague currentStep={step} setStep={setStep}/> */}
           {/* <JoinUserLeague currentStep={step} setStep={setStep}/> */}
