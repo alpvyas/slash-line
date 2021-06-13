45101be7f4c8062b137ec729ae70084657d96987
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-12 21:08:42 -0700

    playing around with mui

--- react-app/src/components/GettingStarted/index.js
+++ react-app/src/components/GettingStarted/index.js
@@ -98,89 +98,65 @@ QontoStepIcon.propTypes = {
   completed: PropTypes.bool,
 };
 
-const ColorlibConnector = withStyles({
-  alternativeLabel: {
-    top: 22,
-  },
-  active: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  completed: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  line: {
-    height: 3,
-    border: 0,
-    backgroundColor: '#eaeaf0',
-    borderRadius: 1,
-  },
-})(StepConnector);
+// const useColorlibStepIconStyles = makeStyles({
+//   root: {
+//     backgroundColor: '#ccc',
+//     zIndex: 1,
+//     color: '#fff',
+//     width: 50,
+//     height: 50,
+//     display: 'flex',
+//     borderRadius: '50%',
+//     // justifyContent: 'space-around',
+//     // alignItems: 'space-around',
+//   },
+//   active: {
+//     backgroundImage:
+//       'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+//     boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+//   },
+//   completed: {
+//     backgroundImage:
+//       'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+//   },
+// });
 
-const useColorlibStepIconStyles = makeStyles({
-  root: {
-    backgroundColor: '#ccc',
-    zIndex: 1,
-    color: '#fff',
-    width: 50,
-    height: 50,
-    display: 'flex',
-    borderRadius: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  active: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  },
-  completed: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-  },
-});
+// function ColorlibStepIcon(props) {
+//   const classes = useColorlibStepIconStyles();
+//   const { active, completed } = props;
 
-function ColorlibStepIcon(props) {
-  const classes = useColorlibStepIconStyles();
-  const { active, completed } = props;
+//   const icons = {
+//     1: <SettingsIcon />,
+//     2: <GroupAddIcon />,
+//     3: <VideoLabelIcon />,
+//   };
 
-  const icons = {
-    1: <SettingsIcon />,
-    2: <GroupAddIcon />,
-    3: <VideoLabelIcon />,
-  };
+//   return (
+//     <div
+//       className={clsx(classes.root, {
+//         [classes.active]: active,
+//         [classes.completed]: completed,
+//       })}
+//     >
+//       {icons[String(props.icon)]}
+//     </div>
+//   );
+// }
 
-  return (
-    <div
-      className={clsx(classes.root, {
-        [classes.active]: active,
-        [classes.completed]: completed,
-      })}
-    >
-      {icons[String(props.icon)]}
-    </div>
-  );
-}
-
-ColorlibStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   */
-  active: PropTypes.bool,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   */
-  completed: PropTypes.bool,
-  /**
-   * The label displayed in the step icon.
-   */
-  icon: PropTypes.node,
-};
+// ColorlibStepIcon.propTypes = {
+//   /**
+//    * Whether this step is active.
+//    */
+//   active: PropTypes.bool,
+//   /**
+//    * Mark the step as completed. Is passed to child components.
+//    */
+//   completed: PropTypes.bool,
+//   /**
+//    * The label displayed in the step icon.
+//    */
+//   icon: PropTypes.node,
+// };
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -315,8 +291,8 @@ const GettingStarted = () => {
             >
 
             {steps.map((label, index) => {
-              // const stepProps = {};
-              // const labelProps ={};
+              const stepProps = {};
+              const labelProps ={};
               // if (isStepOptional(index)) {
               //   labelProps.optional = <Typography variant="caption">Optional</Typography>;
               // }
@@ -328,8 +304,8 @@ const GettingStarted = () => {
           
                   // <Step key={label} {...stepProps}>
                   <Step key={label}>
-                    {/* <StepLabel {...labelProps} StepIconComponent={QontoStepIcon}>{label}</StepLabel> */}
-                    <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+                    <StepLabel {...labelProps} StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+                    <StepLabel>{label}</StepLabel>
                   </Step>
                 
               );
