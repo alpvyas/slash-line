226095abd244a66563f3c695edbc6834262b5f15
Alpesh Vyas <alpesh@Alpeshs-iMac.local>
2021-06-19 17:44:43 -0700

    setting up join user league route and verification

--- react-app/src/components/GettingStarted/JoinUserLeague/index.js
+++ react-app/src/components/GettingStarted/JoinUserLeague/index.js
@@ -3,10 +3,17 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import './JoinUserLeague.css';
+import { useDispatch } from 'react-redux';
 
 const JoinUserLeague = () => {
+  const dispatch = useDispatch();
 
   const [credentials, setCredentials] = useState(undefined);
+
+  const joinLeague = () => {
+    dispatch()
+
+  };
 
   return (
    
