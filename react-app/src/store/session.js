74a5a0c2b010ee8d52f164bd1e89ec3c8a916b90
Alpesh Vyas <alpesh@Alpeshs-iMac.attlocal.net>
2021-08-12 23:06:53 -0700

    added a function to get rid of cookies when user logs out (session and csrf_token)

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -67,7 +67,13 @@ export const logout = () => async (dispatch) => {
     },
   });
 
-  // console.log("RESPONSE LOGOUT: ", response)
+  //deleting cookies on client-side in case connection to server is lost
+  const cookies = document.cookie.split(";");
+  cookies.forEach((cookie) => {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  })
 
   dispatch(removeUser());
   
